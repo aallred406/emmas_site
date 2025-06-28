1c49dc643fe5dc17f9f26754e5549f68226289de
Ashley Allred <a.allredmt@gmail.com>
2025-06-27 22:36:50 -0600

    add hero image

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+// Image file declarations
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.png";
+declare module "*.webp";
+declare module "*.svg";
