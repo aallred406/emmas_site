43d57ad26f14dbd9be38eb4281917e750d28c53a
Ashley Allred <a.allredmt@gmail.com>
2025-06-27 22:36:12 -0600

    update main nav

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -7,18 +7,22 @@ const Navigation = () => {
   const navItems = [
     { path: "/", label: "Home" },
     { path: "/about", label: "About" },
-    { path: "/gallery", label: "Gallery" },
+    { path: "/gallery", label: "Works" },
   ];
 
+  const focusStyles =
+    "focus-visible:rounded-sm focus-visible:ring-1 focus-visible:ring-gray-600 focus-visible:ring-offset-1 focus:outline-0 focus-visible:bg-gray-100";
+  const hoverStyles = "hover:rounded-sm hover:text-gray-900 hover:bg-gray-100";
+
   return (
-    <nav className="bg-white py-4">
+    <nav className="bg-white py-4 border-b-2 border-gray-600">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16">
+        <div className="flex justify-between">
           <div className="flex items-center">
-            <Link to="/">
+            <Link to="/" className={` ${focusStyles}`}>
               <Logo />
             </Link>
-            <p className="text-2xl font-aboreto">EMMA LARHEA PHOTOGRAPHY</p>
+            {/* <p className="text-2xl font-aboreto">EMMA LARHEA PHOTOGRAPHY</p> */}
           </div>
 
           <div className="flex items-center space-x-8">
@@ -26,10 +30,10 @@ const Navigation = () => {
               <Link
                 key={item.path}
                 to={item.path}
-                className={`px-3 py-2 rounded-md text-sm font-medium transition-colors ${
+                className={`px-3 py-2 text-sm font-aboreto transition-colors ${
                   location.pathname === item.path
-                    ? "text-blue-600 bg-blue-50"
-                    : "text-gray-700 hover:text-blue-600 hover:bg-gray-50"
+                    ? `text-gray-600 border-b-2 border-gray-600 font-bold ${hoverStyles} ${focusStyles}`
+                    : `text-gray-700 ${hoverStyles} ${focusStyles}`
                 }`}
               >
                 {item.label}
